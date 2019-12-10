7933c8e6ebd45e4a06ed49ce8fcda96f782a2394
qixianyu-buaa <qixianyu@buaa.edu.cn>
2019-12-10 11:51:45 +0800

    update segment tree

--- include/tree/MySegmentTree.hpp
+++ include/tree/MySegmentTree.hpp
@@ -15,23 +15,16 @@
 // 区间操作: 染色和查询
 // 区间的统计查询
 
-template <typename T, typename Merger>
+template <typename T>
+struct Node;
+
+template <typename T, template <typename U> class Node, template <typename N> class Merger>
 class MySegmentTree
 {
 public:
-        struct Node
-        {
-                Node() : isUsed_(false)
-                {
-                }
-                Node(T val) : val_(val), isUsed(true) {}
-
-                bool isUsed_;
-                T val_;
-        };
 private:
-        std::vector<Node> tree_;
-        std::vector<Node> array_;
+        std::vector<Node<T>> tree_;
+        std::vector<Node<T>> array_;
 
 public:
         MySegmentTree(std::vector<T> &v)
@@ -39,26 +32,30 @@ public:
                 array_.reserve(v.size());
                 for (int i = 0; i < v.size(); ++i)
                 {
-                        array_.push_back(Node(v.at(i)));
+                        array_.push_back(Node<T>(v.at(i)));
                 }
 
                 tree_.reserve(4 * v.size());
+                for (int i = 0; i < 4 * v.size(); ++i)
+                {
+                        tree_.push_back(Node<T>());
+                }
                 buildSegmentTree(0, 0, array_.size() - 1);
         }
 
-        int getLeftChild(int index)
+        int getLeftChild(int i)
         {
                 return 2 * i + 1;
         }
 
-        int getRightChild(int index)
+        int getRightChild(int i)
         {
                 return 2 * i + 2;
         }
 
-        int getParent(int index)
+        int getParent(int i)
         {
-                assert(index > 0);
+                assert(i > 0);
                 return (i - 1) / 2;
         }
 
@@ -67,18 +64,81 @@ public:
         {
                 if (l == r)
                 {
-                        tree_.at(index) = array_.at(index);
+                        tree_.at(index) = array_.at(l);
                         return;
                 }
-                int left = getLeftChild(index);
-                int right = getRightChild(index);
+                int left_child = getLeftChild(index);
+                int right_child = getRightChild(index);
 
-                int mid = left + (right - left) / 2;
+                int m = l + (r - l) / 2;
 
-                buildSegmentTree(left, left, mid);
-                buildSegmentTree(right, mid + 1, right);
+                // 递归到底
+                buildSegmentTree(left_child, l, m);
+                buildSegmentTree(right_child, m + 1, r);
 
-                tree_.at(index) = Merger(tree_.at(left), tree_.at(right));
+                //  向上合并
+                tree_.at(index) = Merger<Node<T>>()(tree_.at(left_child), tree_.at(right_child));
+        }
+
+        T query(int query_l, int query_r)
+        {
+                return query(0, 0, array_.size() - 1, query_l, query_r);
+        }
+
+        T query(int tree_index, int l, int r, int query_l, int query_r)
+        {
+                if (query_l == l && query_r == r)
+                {
+                        return tree_.at(tree_index).val_;
+                }
+
+                int left_child = getLeftChild(tree_index);
+                int right_child = getRightChild(tree_index);
+
+                int mid = l + (r - l) / 2; // mid 为树表示的区间的中点
+
+                //  只需要查找右侧
+                if (query_l >= mid + 1)
+                        return query(right_child, mid + 1, r, query_l, query_r);
+
+                //  只需要查找左侧
+                if (query_r <= mid)
+                        return query(left_child, l, mid, query_l, query_r);
+
+                T l_val = query(left_child, l, mid, query_l, mid);
+                T r_val = query(right_child, mid, r, mid, query_r);
+                Node<T> left_node = Node<T>(l_val);
+                Node<T> right_node = Node<T>(r_val);
+                Node<T> res = Merger<Node<T>>()(left_node, right_node);
+                return res.val_;
+        }
+
+        void set(int index, T val)
+        {
+                array_.at(index).val_ = val;
+                set(0, 0, array_.size() - 1, index, val);
+        }
+
+        void set(int tree_index, int l, int r, int index, T val)
+        {
+                if (l == r)
+                {
+                        tree_.at(tree_index).val_ = val;
+                        return;
+                }
+
+                int mid = l + (r - l) / 2;
+                int left_child = getLeftChild(tree_index);
+                int right_child = getRightChild(tree_index);
+
+                if (index >= mid + 1)
+                        set(right_child, mid + 1, r, index, val);
+
+                if (index <= mid)
+                        set(left_child, l, mid, index, val);
+
+                Node<T> res = Merger<Node<T>>()(tree_.at(left_child), tree_.at(right_child));
+                tree_.at(tree_index) = res;
         }
 
         T get(int index)
@@ -96,9 +156,9 @@ public:
                 std::cout << "[";
                 for (int i = 0; i < tree_.size(); ++i)
                 {
-                        if (tree_[i].isUsed)
+                        if (tree_[i].isUsed_)
                         {
-                                std::cout << tree[i];
+                                std::cout << tree_[i].val_;
                         }
                         else
                         {
--- test/test_seg_tree.cpp
+++ test/test_seg_tree.cpp
@@ -2,7 +2,48 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+class Node
+{
+public:
+        Node() : isUsed_(false)
+        {
+        }
+        Node(T val) : val_(val), isUsed_(true) {}
+
+        bool isUsed_;
+        T val_;
+};
+
+template <typename Node>
+class Merger
+{
+public:
+        Node operator()(Node &lhs, Node &rhs)
+        {
+                if (lhs.isUsed_ && rhs.isUsed_)
+                        return Node(lhs.val_ + rhs.val_);
+
+                if (lhs.isUsed_)
+                        return lhs;
+
+                if (rhs.isUsed_)
+                        return rhs;
+
+                return Node();
+        }
+};
 
 int main()
 {
+        int arr[] = {1, 3, 5};
+        std::vector<int> v(begin(arr), end(arr));
+        std::cout << v.size() << std::endl;
+        MySegmentTree<int, Node, Merger> seg_tree(v);
+        seg_tree.print();
+
+        std::cout << seg_tree.query(0, 1) << std::endl; //4
+
+        seg_tree.set(2, 10);
+        seg_tree.print();
 }
